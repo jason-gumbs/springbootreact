ac1b70940093fe35f2b2bc8e934ad9fc4fae9a20
DenimCity <altidorj1@gmail.com>
2018-06-11 09:30:29 -0400

    created basic styles

---
+++ client/src/components/styledComponents/basicStyles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+display:flex;
+justify-content:center;
+`
+export const Title = styled.h1`
+text-align:center;
+font-size: 3.5vh;
+`
+
+export const Loading = styled.div `
+display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+`
