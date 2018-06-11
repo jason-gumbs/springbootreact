522aed5f41d55b63f09ac113654b8910c81100ca
DenimCity <altidorj1@gmail.com>
2018-06-11 09:04:43 -0400

    made the login page the landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,27 @@
-import React from "react";
-import OmdbContainer from "./components/OmdbContainer";
+import React, { Component } from "react";
+import LoginPage from "./components/LoginPage";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-const App = () => <OmdbContainer />;
 
+
+class App extends Component {
+
+
+    render() {
+        return (
+            <div>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={LoginPage} />
+                    {/* <Route exact path="/" component={LandingPage} />
+                    <Route exact path="/doors" component={DoorsList} />
+                    <Route exact path="/doors/create" component={Form} />
+                    <Route exact path="/doors/:doorId" component={SingleDoor} /> */}
+                </Switch>
+            </Router>
+            </div>
+        )
+    }
+
+}
 export default App;
---
+++ client/src/components/LoginPage.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react"
+import {Redirect} from 'react-router-dom'
+import Container from "./Container"
+import Row from "./Row"
+import Col from "./Col"
+import Panel from "./Panel"
+import Search from "./Search"
+import * as API from "../utils/API"
+
+
+export default class LoginPage extends Component {
+ 
+    state = {
+    username:"",
+    password:"",
+    result:"",
+    LoggedIn:false
+  }
+  
+
+  handleInputChange = event => {
+    const value = event.target.value
+    const name = event.target.name
+    this.setState({
+      [name]: value
+    })
+  }
+
+  // When the form is submitted, search the OMDB API for the value of `this.state.search`
+  handleFormSubmit = event => {
+    event.preventDefault()
+    console.log("hey")
+    API.createUser({  username: this.state.username,
+      password: this.state.password})
+      .then(res => {
+        if(!res.data){
+          document.getElementById("result").innerHTML = "<p>username exist</p>"
+          //swal(`username exist`)
+        }else{
+          document.getElementById("result").innerHTML = "<p >thank you are all signed up</p>"
+         //swal(`thank you are all signed up`)
+         
+         }
+ }) 
+ .catch(err => console.log(err))
+ }
+
+ handleloginSubmit = event => {
+  event.preventDefault()
+  console.log("raqqq")
+  API.login({  username: this.state.username,
+    password: this.state.password})
+    .then(res => {
+      if(!res.data){
+        document.getElementById("result").innerHTML = "<p>wrong username/password</p>"
+      
+      }else{document.getElementById("result").innerHTML = "<p>`you are loged in</p>"
+      this.setState({LoggedIn})
+       }
+
+    })
+   
+    .catch(err => console.log(err))
+
+  
+}
+
+  render() {
+    if(this.state.LoggedIn){
+      return <Redirect to="/doors"/>
+    }
+    return (
+      <Container>
+        <Row>
+          
+          <Col size="md-4" className="m-auto">
+            <Panel heading="Sign up/Login">
+              <Search
+                handleInputChange={this.handleInputChange}
+                handleFormSubmit={this.handleFormSubmit}
+                
+              />
+        <div id= "result">
+        </div>
+    
+            </Panel>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
+
+ 
