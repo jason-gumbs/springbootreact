578a8ad618274ce4ac9e0a7b0aa55ff094a39722
DenimCity <altidorj1@gmail.com>
2018-06-11 15:46:25 -0400

    update

--- server/src/main/java/com/example/demo/controller/userController.java
+++ server/src/main/java/com/example/demo/controller/userController.java
@@ -4,16 +4,13 @@ package com.example.demo.controller;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.LinkedList;
 import java.util.List;
 
-
+@CrossOrigin
 @RestController
 public class userController {
 
